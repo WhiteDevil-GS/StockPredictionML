11e20fc4c5190aff0448d258593803a661a09b9e
Ganesh Soppin <128776329+WhiteDevil-GS@users.noreply.github.com>
2025-05-29 19:25:21 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 app.use(
   cors({
     origin: [
